6fb25a1c88bcc82f9113961354890339ee54f734
AshutoshRaval <122703876+AshutoshRaval@users.noreply.github.com>
2024-07-27 21:31:32 -0400

    version1

---
+++ blog/urls.py
@@ -0,0 +1,23 @@
+# from django.urls import path
+# from . import views
+
+# urlpatterns = [
+#     path('', views.post_list, name='post_list'),
+# ]
+
+
+from django.urls import path
+from . import views
+from .views import PostListCreate, PostDetail
+
+urlpatterns = [
+    path('', views.post_list, name='post_list'),
+    path('post/new/', views.post_new, name='post_new'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    path('post/<int:pk>/delete/', views.post_delete, name='post_delete'),
+    path('api/posts/', PostListCreate.as_view(), name='post_list_create'),
+    path('api/posts/<int:pk>/', PostDetail.as_view(), name='post_detail'),
+]
+
+
